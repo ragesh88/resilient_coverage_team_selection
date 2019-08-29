8011946a6e54a941129ca6b0bb0de856af84b110
ragesh <rageshku@asu.edu>
2019-08-29 14:27:08 -0700

    MILP solver for problem 3 added

---
+++ prob3_MILP.m
@@ -0,0 +1,41 @@
+function [info,rob_sel] = prob3_MILP(areas, R_val, alpha, d_area)
+% This function takes the inputs of the MILP problem for problem three and
+% splits out the binary solution for the problem 
+%INPUTs
+% areas : the vector containing the area covered by each robot
+% R_val : the vector containing the reliablility function values
+% alpha : max failure probability
+% d_area : desired area coverage
+
+%   Detailed explanation goes here
+% the function solves a mixed integer linear programing problem using the
+% solver Mosek
+
+Tot_agents = length(areas);
+
+prob.c = ones(1, Tot_agents);
+prob.a = [-log(1-R_val'); areas'];
+prob.blc = [-log(alpha) d_area];
+prob.buc = [inf inf];
+prob.blx = zeros(1, Tot_agents);
+prob.bux = ones(1, Tot_agents);
+
+prob.ints.sub = 1:Tot_agents;
+
+% parameter list 
+param.MSK_IPAR_LOG = 0;
+param.MSK_IPAR_NUM_THREADS =  10;
+
+% Optimize the problem.
+[~,res] = mosekopt('minimize',prob,param);
+try
+% Display the optimal solution.
+% res.sol.int
+info = res.rcodestr;
+rob_sel = res.sol.int.xx;
+catch
+fprintf('MSKERROR: Could not get solution')
+end
+
+
+end
