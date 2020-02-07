02b5399a5280a99ef7432568f5d09793e84135d5
ragesh88 <ragesh88@gmail.com>
2020-02-06 20:38:42 -0800

    working init coord gen

---
+++ experiments/exp_init_coord.m
@@ -0,0 +1,53 @@
+% 
+
+function [outputArg1,outputArg2] = exp_init_coord(A_n,inputArg2)
+%the function computes the initial coordinates for the robot experiment
+%   Detailed explanation goes here
+% initialize the system with the parameters
+% simulation parameters
+
+% the vector indicating the available robots
+Rob_pool = ones(A_n,1);
+
+
+exp_parameters;
+
+% add paths from the previous folder
+addpath('..');
+
+% compute the coordinate of the robots
+
+% solve the MILP to select the robots
+% [info, Rob_sel] = prob1_MILP(Rob_costs,Rob_areas,Rob_vals,budget,...
+%     alpha,domain_area);
+
+% select all robots
+
+Rob_sel = Rob_pool;
+
+% the set containing the selected robot labels
+Rob_sel_labels = find(Rob_sel);
+
+
+% bounding box of the domain
+b_box = [env_x(1) env_x(end)
+    env_y(1) env_y(end)];
+
+
+% solve the greedy algorithm to place the robots
+[set_gre, h_gre, ~] = gre_place(Rob_sel_labels, R_x, delta,...
+    b_box, Rob_sen_rads,[],[]);
+
+% update the list of available robots
+% Rob_pool = Rob_pool - Rob_sel;
+% update the list of active robots
+Rob_active = Rob_sel;
+% labels of the active robots
+Rob_active_lab = find(Rob_active);
+% positions of active robots
+Rob_active_pos = set_gre;
+
+
+outputArg1 = A_n;
+outputArg2 = inputArg2;
+end
---
+++ experiments/exp_parameters.m
@@ -0,0 +1,63 @@
+% The parameter for the experiment
+
+% global variables
+global lambda com_range
+
+
+
+% tuning parameter
+omega = 2; % hoops
+radius_tune = 10; % radius to consider for tuning
+coverage_thres = 0.20;
+
+% realiability parameters
+% generate mean failure time for all robots
+MTTF_mean = 420;
+MTTFs = MTTF_mean*ones(A_n,1);
+% max time of the mission
+T_max = max(MTTFs);
+% time span of the mission
+T = [0 T_max];
+% lambda zero in the reliability model
+l0s = 1./(10*MTTFs);
+% k in the reliability model
+k = 0.0001 * l0s;
+% required reliability
+alpha = 0.3;
+
+
+% Cost parameters
+% the cost of each robot
+max_cost = 50;
+Rob_costs = max_cost*MTTFs/max(MTTFs);
+budget = 500;
+
+% environment parameters
+env_size = 4; % environment size
+env_min_x = -2;
+env_min_y = -2;
+env_max_x = 2;
+env_max_y = 2;
+R_x = 1; % unifrom density 
+delta = 2; % discretization parameter
+domain_area = env_size^2;
+env_x = env_min_x:delta:env_max_x;
+env_y = env_min_y:delta:env_max_y;
+
+% area covered by each robot
+max_area = 0.5;
+Rob_areas = max_area *MTTFs/max(MTTFs);
+% sensing radius of the robots
+Rob_sen_rads = sqrt(Rob_areas/pi);
+% sensing parameter 
+lambda = 0.1; % sensing decay parameter 
+% communication range
+com_range = max(Rob_sen_rads);
+
+% compute the reliability value for all
+Rob_vals = zeros(A_n,1);
+for i = 1:A_n
+    Rob_vals(i) = reliability(T,l0s(i),k(i));
+end
+l_R_vals = -log(1-Rob_vals);
+l_alpha = -log(alpha);
